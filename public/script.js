7375b32d6715369595b368f9fff4b93d88687413
takatama <takatama@users.noreply.github.com>
2024-11-17 18:55:00 +0900

    Refactored code to enhance error handling and improve Turnstile integration in the frontend.

--- public/script.js
+++ public/script.js
@@ -2,43 +2,9 @@ const translateBtn = document.getElementById("translate-btn");
 const generateBtn = document.getElementById("generate-btn");
 const translateSpinner = document.getElementById("translate-spinner");
 const generateSpinner = document.getElementById("generate-spinner");
-const translateErrorMessage = document.getElementById(
-  "translate-error-message"
-);
-const generateErrorMessage = document.getElementById("generate-error-message");
 
 translateBtn.addEventListener("click", translateText);
 generateBtn.addEventListener("click", generateImage);
-
-async function handleTurnstileResponse(token) {
-  const formData = new FormData();
-  formData.set("cf-turnstile-response", token);
-  const response = await fetch("/auth", {
-    method: "POST",
-    body: formData,
-  });
-  if (!response.ok) {
-    throw new Error("認証に失敗しました");
-  }
-  translateBtn.disabled = false;
-  generateBtn.disabled = false;
-}
-
-function initializeTurnstile() {
-  turnstile.render("#turnstile-widget", {
-    // 自分で作成したTurnstileのSite Keyを記入してください
-    sitekey: "0x4AAAAAAAzs018lIIK5s9-R",
-    callback: handleTurnstileResponse,
-    "expired-callback": resetToTurnstile,
-  });
-}
-
-function resetToTurnstile() {
-  translateBtn.disabled = true;
-  generateBtn.disabled = true;
-  isAuthenticated = false;
-  turnstile.reset();
-}
 
 function toggleButton(button, spinner, isLoading) {
   button.disabled = isLoading;
@@ -73,7 +39,8 @@ async function handleEventStream(response, textarea) {
 }
 
 async function translateText() {
-  translateErrorMessage.style.display = "none";
+  const errorMessage = document.getElementById("translate-error-message");
+  errorMessage.style.display = "none";
   toggleButton(translateBtn, translateSpinner, true);
   const promptInput = document.getElementById("prompt");
 
@@ -85,54 +52,44 @@ async function translateText() {
       body: formData,
       credentials: "include",
     });
-    if (!response.ok) {
-      if (response.status === 401) {
-        translateErrorMessage.textContent =
-          "セッションの有効期限が切れました。もう一度試してください。";
-        translateErrorMessage.style.display = "block";
-        resetToTurnstile();
-      } else {
-        const errorData = await response.json();
-        throw new Error("エラー:", errorData.error);
-      }
-    }
+
+    // if (!response.ok) {
+    //   const errorData = await response.json();
+    //   throw new Error("エラー:", errorData.error);
+    // }
+    await verifyResponse(response);
+
     const translatedPrompt = document.getElementById("translated-prompt");
     await handleEventStream(response, translatedPrompt);
   } catch (error) {
     console.error("エラー: ", error);
-    translateErrorMessage.textContent =
-      "翻訳に失敗しました。もう一度試してください。";
-    translateErrorMessage.style.display = "block";
+    errorMessage.textContent = "翻訳に失敗しました。もう一度試してください。";
+    errorMessage.style.display = "block";
   } finally {
     toggleButton(translateBtn, translateSpinner, false);
   }
 }
 
 async function generateImage() {
-  generateErrorMessage.style.display = "none";
+  const errorMessage = document.getElementById("generate-error-message");
+  errorMessage.style.display = "none";
   toggleButton(generateBtn, generateSpinner, true);
   const translatedPrompt = document.getElementById("translated-prompt");
 
   try {
     const formData = new FormData();
     formData.set("prompt", translatedPrompt.value);
-
     const response = await fetch(`/generate-image`, {
       method: "POST",
       body: formData,
       credentials: "include",
     });
 
-    if (!response.ok) {
-      if (response.status === 401) {
-        generateErrorMessage.textContent =
-          "セッションの有効期限が切れました。もう一度試してください。";
-        generateErrorMessage.style.display = "block";
-        resetToTurnstile();
-      } else {
-        throw new Error("画像生成エラー");
-      }
-    }
+    // if (!response.ok) {
+    //   const errorData = await response.json();
+    //   throw new Error("エラー:", errorData.error);
+    // }
+    await verifyResponse(response);
 
     const data = await response.json();
     if (data.imageUrl) {
@@ -144,9 +101,9 @@ async function generateImage() {
     }
   } catch (error) {
     console.error("エラー: ", error);
-    generateErrorMessage.textContent =
+    errorMessage.textContent =
       "画像生成に失敗しました。もう一度試すか、プロンプトを変えてください。";
-    generateErrorMessage.style.display = "block";
+    errorMessage.style.display = "block";
   } finally {
     toggleButton(generateBtn, generateSpinner, false);
   }
